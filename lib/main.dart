a69a632364d7d52eb7406b9eab68f8afeafbe7b4
varungolusupudi <varungolusupudi@gmail.com>
2024-02-14 23:10:35 -0800

    updated main and firebase_options

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,55 @@
 // Importing material.dart, which contains Flutter's Material Design widgets and theme.
 import 'package:flutter/material.dart';
+import 'package:slugnutrition/src/features/authentication/controllers/signup_controller.dart';
 import '/src/utils/theme/theme.dart';
 import 'package:get/get.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import '/src/features/authentication/screens/splash_screen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '/src/features/authentication/screens/welcome/welcome_screen.dart';
+import '/src/features/authentication/screens/on_boarding/on_boarding_screen.dart';
+import 'firebase_options.dart';
+import '/src/repository/authentication_repository/authentication_repository.dart';
+import '/src/repository/user_repository/user_repository.dart';
+import '/src/features/authentication/screens/signup/signup_screen.dart';
+import '/src/features/authentication/controllers/signup_controller.dart';
+
+// import 'app.dart';
+
+// /// ------ For Docs & Updates Check ------
+// /// ------------- README.md --------------
+//
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized(); // Required for async operations before runApp
+//
+//   // Initialize Firebase
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//
+//   // Check if onboarding is completed
+//   SharedPreferences prefs = await SharedPreferences.getInstance();
+//   bool onboardingComplete = prefs.getBool('onboardingComplete') ?? false;
+//
+//   // Use GetX for navigation to the appropriate screen based on onboarding completion
+//   runApp(GetMaterialApp(
+//     home: onboardingComplete ? WelcomeScreen() : OnBoardingScreen(),
+//   ));
+// }
 
 // The main entry point of the application. The arrow syntax is used for one-liner functions.
-void main() => runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  Get.put(AuthenticationRepository());
+  Get.put(UserRepository());
+  Get.put(SignUpController());
+  runApp(const App());
+}
+
 
 // The App class is a StatelessWidget, which means its properties (or configuration) cannot change.
 // All StatelessWidget must override the build method.
