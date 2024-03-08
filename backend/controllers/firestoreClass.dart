43dd23eadcfbddc1d566161c4a61278c9a1632c6
An-geles <66815800+An-geles@users.noreply.github.com>
2024-03-08 12:23:55 -0800

    update: file linking

--- backend/controllers/firestoreClass.dart
+++ backend/controllers/firestoreClass.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'menu_item.dart';
+import 'menuItemClass.dart';
 
 class FirestoreService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
